41abae4cb9f5125a01414e2fad3266adb990a92a
Saunak Singha <singhasaunak@gmail.com>
2026-05-04 01:27:33 +0530

    Add my event class

---
+++ 1. Fundamentals/MyEventClass.js
@@ -0,0 +1,16 @@
+const EventEmitter = require("events");
+
+class Chat extends EventEmitter {
+  sendMessage(message) {
+    console.log("Testing");
+    console.log(`Sending message ${message}`);
+    this.emit("sendMessage", message);
+  }
+}
+
+const chat = new Chat();
+chat.on("sendMessage", (message) => {
+  console.log(`Message has been sent : ${message}`);
+});
+
+chat.sendMessage("New message");
--- 1. Fundamentals/MyEvents.js
+++ 1. Fundamentals/MyEvents.js
@@ -6,4 +6,10 @@ eventEmitter.on("welcome", (name) => {
   console.log(`Welcome ${name}`);
 });
 
+eventEmitter.once("pushAlert", (alertCode) => {
+  console.log(`Will only work once ${alertCode}`);
+});
+
 eventEmitter.emit("welcome", "Saunak");
+eventEmitter.emit("pushAlert", 1);
+eventEmitter.emit("pushAlert", 2);
